4309a687db111a990d8d9a5d19d9cc71020d3e7c
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2024-12-11 07:51:34 +0000

    wrapping paper useable as fuel

---
+++ Contents/mods/ChristmasPresents/media/lua/server/ChristmasPresents/ItemDefinitons.lua
@@ -0,0 +1,5 @@
+-- grrr short type
+campingFuelType["WrappingPaperRipped"] = 2/60
+campingLightFireType["WrappingPaperRipped"] = 2/60
+campingFuelType["WrappingPaper"] = 2/60
+campingLightFireType["WrappingPaper"] = 2/60
